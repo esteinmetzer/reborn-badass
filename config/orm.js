4341de5d3d50c5590c5a9b9b57fd68e57bbb629b
Matt Benson <rabenson6@msn.com>
2020-03-26 20:41:54 -0700

    added truckcount function

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,7 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-let prj_pllts = 'SELECT *, ((prj_units * 1.00) / units_pllt) AS "prj_pllts" FROM item;'
+
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
